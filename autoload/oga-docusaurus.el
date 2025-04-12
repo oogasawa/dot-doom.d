1b2611dd3facc9e511ec4ee03f913f0c1e284723
Osamu Ogasawara <osamu.ogasawara@gmail.com>
2025-04-12 09:02:15 +0900

    Remove the buggy funcitions

--- autoload/oga-docusaurus.el
+++ autoload/oga-docusaurus.el
@@ -2,59 +2,44 @@
 
 ;;;###autoload
 (defun oga/docusaurus-convert-from-chatgpt (start end)
-  "Clean up ChatGPT-generated Markdown content in the selected region.
-This includes symbol replacements and removal of emojis from headings and list items."
+  "Perform a series of replacements in the selected region.
+START and END specify the region boundaries."
   (interactive "r")
   (save-restriction
     (narrow-to-region start end)
     (goto-char (point-min))
-
     ;; Replace ** with an empty string
     (while (search-forward "**" nil t)
       (replace-match "" nil t))
-
     ;; Replace \( with $
     (goto-char (point-min))
     (while (search-forward "\\(" nil t)
       (replace-match "$" nil t))
-
     ;; Replace \) with $
     (goto-char (point-min))
     (while (search-forward "\\)" nil t)
       (replace-match "$" nil t))
-
     ;; Replace \[ with $$
     (goto-char (point-min))
     (while (search-forward "\\[" nil t)
       (replace-match "$$" nil t))
-
     ;; Replace \] with $$
     (goto-char (point-min))
     (while (search-forward "\\]" nil t)
       (replace-match "$$" nil t))
-
     ;; Replace ^--- with an empty string using regex
     (goto-char (point-min))
     (while (re-search-forward "^---" nil t)
       (replace-match "" nil nil))
-
-    ;; Remove unwanted glyphs
     (goto-char (point-min))
     (while (re-search-forward "" nil t)
       (replace-match "" nil nil))
-
     (goto-char (point-min))
-    (while (re-search-forward "" nil t)
+    (while (re-search-forward "citeturn.+search.+" nil t)
       (replace-match "" nil nil))
 
-    ;; Remove emoji-like characters from headings and list items
-    ;; Match lines starting with #, -, *, + followed by emoji symbols
-    (goto-char (point-min))
-    (while (re-search-forward
-            "^\\([ \t]*\\(?:[#]+\\|[-*+]\\)[ \t]+\\)\\(?:[\u2190-\u21FF\u2300-\u23FF\u2600-\u27BF\U0001F000-\U0001FAFF][[:space:]]*\\)+"
-            nil t)
-      (replace-match "\\1" nil nil))
     ))
+
 
 
 ;;;###autoload
